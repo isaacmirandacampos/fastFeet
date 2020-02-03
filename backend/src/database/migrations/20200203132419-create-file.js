1074d15bed0da1915487a778d5e6fd2b932b5467
Isaac Miranda Campos <isaacdmcampos@outlook.com>
2020-02-03 10:34:31 -0300

    Add migration file

---
+++ backend/src/database/migrations/20200203132419-create-file.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      return queryInterface.createTable('files', 
+      { 
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        path: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+        update_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        }
+      });
+  },
+
+  down: (queryInterface) => {
+
+      return queryInterface.dropTable('files');
+  }
+};
